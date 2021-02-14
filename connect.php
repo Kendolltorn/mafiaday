9d35749b50bc171187477717daa95781dd044bd5
Kendolltorn <79059692+Kendolltorn@users.noreply.github.com>
2021-02-14 14:18:24 -0600

    connect changes

--- connect.php
+++ connect.php
@@ -1,11 +1,11 @@
 <? 
-$mysql_server = "ec2-52-7-168-69.compute-1.amazonaws.com"; // localhost is common on most hosts.
+$mysql_server = "ec2-67-202-63-147.compute-1.amazonaws.com"; // localhost is common on most hosts.
 
-$mysql_user = "akbyftixfsaoax"; //  this is the name of your username of the server.
+$mysql_user = "brozlpvznlqhjo"; //  this is the name of your username of the server.
 
-$mysql_password = "d7417177107231348bf0ff8258e7a4af5b03c71cc684dfbaa8bfa6768895a2dd"; // the password connected to the username. MAKE IT COMPLEX.
+$mysql_password = "31cb511819fd5f9108372fdb7bdc8e4c2bcbb813dd7076a82eaaced410b4e8ed"; // the password connected to the username. MAKE IT COMPLEX.
 
-$mysql_database = "dbq90e1fhlhh5"; // the database name of where to connect to and where the information will be help.
+$mysql_database = "d9p794pmkr7vi3"; // the database name of where to connect to and where the information will be help.
 
 $connection = mysql_connect("$mysql_server","$mysql_user","$mysql_password") or die ("Unable to establish a DB connection");
 
